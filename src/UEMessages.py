badf517707d82625f651163c549846d5040abd66
Tariro Mukute <tariromukute@gmail.com>
2023-07-12 20:16:03 +0000

    Structured UE messages scripts for readability

---
+++ src/UEMessages.py
@@ -0,0 +1,151 @@
+from UEUtils import *
+from pycrate_mobile.TS24008_IE import encode_bcd
+from pycrate_mobile.TS24501_IE import FGSIDTYPE_IMEISV
+from pycrate_mobile.NAS import FGMMRegistrationRequest, FGMMMODeregistrationRequest, FGMMRegistrationComplete, FGMMAuthenticationResponse, FGMMSecProtNASMessage, FGMMSecurityModeComplete
+from pycrate_mobile.NAS5G import parse_NAS5G
+from CryptoMobile.Milenage import Milenage, make_OPc
+from CryptoMobile.conv import conv_501_A2, conv_501_A4, conv_501_A6, conv_501_A7, conv_501_A8
+
+def security_prot_encrypt(ue, Msg):
+    IEs = {}
+    IEs['5GMMHeaderSec'] = { 'EPD': 126, 'spare': 0, 'SecHdr': 4 }
+    SecMsg = FGMMSecProtNASMessage(val=IEs)
+    SecMsg['NASMessage'].set_val(Msg.to_bytes())
+    SecMsg.encrypt(key=ue.k_nas_enc, dir=0, fgea=1, seqnoff=0, bearer=1)
+    SecMsg.mac_compute(key=ue.k_nas_int, dir=0, fgia=1, seqnoff=0, bearer=1)
+    return SecMsg
+
+def security_prot_decrypt(Msg, ue):
+    # Msg, err = parse_NAS5G(data)
+    # if err:
+    #     return
+    # check if message is encrypted
+    if Msg['5GMMHeaderSec']['SecHdr'].get_val() == 2:
+        # decrypt message
+        Msg.decrypt(ue.k_nas_enc, dir=1, fgea=1, seqnoff=0, bearer=1)
+        Msg, err = parse_NAS5G(Msg._dec_msg)
+        if err:
+            return None
+        return Msg
+    else:
+        return Msg
+
+def registration_request(ue, IEs, Msg=None):
+    IEs['5GMMHeader'] = {'EPD': 126, 'spare': 0, 'SecHdr': 0, 'Type': 65}
+    IEs['NAS_KSI'] = {'TSC': 0, 'Value': 7}
+    IEs['5GSRegType'] = {'FOR': 1, 'Value': 1}
+    IEs['5GSID'] = {'spare': 0, 'Fmt': 0, 'spare': 0, 'Type': 1, 'Value': {'PLMN': ue.mcc + ue.mnc,
+                                                                           'RoutingInd': b'\x00\x00', 'spare': 0, 'ProtSchemeID': 0, 'HNPKID': 0, 'Output': encode_bcd(ue.msin)}}
+    IEs['UESecCap'] = {'5G-EA0': 1, '5G-EA1_128': 1, '5G-EA2_128': 1, '5G-EA3_128': 1, '5G-EA4': 0, '5G-EA5': 0, '5G-EA6': 0, '5G-EA7': 0,
+                       '5G-IA0': 1, '5G-IA1_128': 1, '5G-IA2_128': 1, '5G-IA3_128': 1, '5G-IA4': 0, '5G-IA5': 0, '5G-IA6': 0, '5G-IA7': 0,
+                       'EEA0': 1, 'EEA1_128': 1, 'EEA2_128': 1, 'EEA3_128': 1, 'EEA4': 0, 'EEA5': 0, 'EEA6': 0, 'EEA7': 0,
+                       'EIA0': 1, 'EIA1_128': 1, 'EIA2_128': 1, 'EIA3_128': 1, 'EIA4': 0, 'EIA5': 0, 'EIA6': 0, 'EIA7': 0}
+    Msg = FGMMRegistrationRequest(val=IEs)
+    ue.MsgInBytes = Msg.to_bytes()
+    logger.debug(f"UE {ue.supi} sending registration_request")
+    ue.set_state(FGMMState.REGISTERED_INITIATED)
+    return Msg
+
+
+def registration_complete(ue, IEs, Msg=None):
+    IEs['5GMMHeader'] = {'EPD': 126, 'spare': 0, 'SecHdr': 0, 'Type': 67}
+    # IEs['SORTransContainer'] = { 'ListInd': }
+    Msg, = FGMMRegistrationComplete(val=IEs)
+    ue.MsgInBytes = Msg.to_bytes()
+    SecMsg = security_prot_encrypt(ue, Msg)
+    logger.debug(f"UE {ue.supi} sending registration_complete")
+    ue.set_state(FGMMState.REGISTERED)
+    return SecMsg
+
+
+def mo_deregistration_request(ue, IEs, Msg=None):
+    IEs['5GMMHeader'] = {'EPD': 126, 'spare': 0, 'SecHdr': 0, 'Type': 69}
+    IEs['NAS_KSI'] = {'TSC': 0, 'Value': 7}
+    IEs['DeregistrationType'] = {'SwitchOff': 0, 'ReregistrationRequired': 0, 'AccessType': 1 }
+    IEs['5GSID'] = {'spare': 0, 'Fmt': 0, 'spare': 0, 'Type': 1, 'Value': {'PLMN': ue.mcc + ue.mnc,
+                                                                           'RoutingInd': b'\x00\x00', 'spare': 0, 'ProtSchemeID': 0, 'HNPKID': 0, 'Output': encode_bcd(ue.msin)}}
+    Msg = FGMMMODeregistrationRequest(val=IEs)
+    ue.MsgInBytes = Msg.to_bytes()
+    logger.debug(f"UE {ue.supi} sending mo_deregistration_request")
+    ue.set_state(FGMMState.DEREGISTERED_INITIATED)
+    return Msg
+
+def deregistration_complete(ue, IEs, Msg=None):
+    ue.set_state(FGMMState.DEREGISTERED)
+    return None
+
+def authentication_response(ue, IEs, Msg):
+    # Msg, err = parse_NAS_MO(data)
+    # if err:
+    #     return
+    OP = unhexlify(ue.op)
+    key = unhexlify(ue.key)
+
+    sqn_xor_ak, amf, mac = Msg['AUTN']['AUTN'].get_val()
+    _, rand = Msg['RAND'].get_val()
+    abba = Msg['ABBA']['V'].get_val()
+
+    Mil = Milenage(OP)
+    if ue.op_type == 'OPC':
+        Mil.set_opc(OP)
+    AK = Mil.f5star(key, rand)
+
+    SQN = byte_xor(AK, sqn_xor_ak)
+
+    Mil.f1(unhexlify(ue.key), rand, SQN=SQN, AMF=amf)
+    RES, CK, IK, _ = Mil.f2345(key, rand)
+    Res = conv_501_A4(CK, IK, ue.sn_name, rand, RES)
+
+    IEs['5GMMHeader'] = {'EPD': 126, 'spare': 0, 'SecHdr': 0, 'Type': 87}
+    IEs['RES'] = Res
+    Msg = FGMMAuthenticationResponse(val=IEs)
+    ue.MsgInBytes = Msg.to_bytes()
+
+    # Note: See CryptoMobile.conv for documentation of this function and arguments
+    # Get K_AUSF
+    ue.k_ausf = conv_501_A2(CK, IK, ue.sn_name, sqn_xor_ak)
+    # Get K_SEAF
+    ue.k_seaf = conv_501_A6(ue.k_ausf, ue.sn_name)
+    # Get K_AMF
+    ue.k_amf = conv_501_A7(ue.k_seaf, ue.supi.encode('ascii'), abba)
+    # Get K_NAS_ENC
+    ue.k_nas_enc = conv_501_A8(ue.k_amf, alg_type=1, alg_id=1)
+    # Get least significate 16 bytes from K_NAS_ENC 32 bytes
+    ue.k_nas_enc = ue.k_nas_enc[16:]
+    # Get K_NAS_INT
+    k_nas_int = conv_501_A8(ue.k_amf, alg_type=2, alg_id=1)
+    ue.set_k_nas_int(k_nas_int)
+    ue.k_nas_int = ue.k_nas_int[16:]
+    
+    logger.debug(f"UE {ue.supi} sending authentication_response")
+    ue.set_state(FGMMState.AUTHENTICATED_INITIATED)
+    return Msg
+    
+def security_mode_complete(ue, IEs, Msg):
+    RegIEs = {}
+    RegIEs['5GMMHeader'] = {'EPD': 126, 'spare': 0, 'SecHdr': 0, 'Type': 65}
+    RegIEs['NAS_KSI'] = {'TSC': 0, 'Value': 7}
+    RegIEs['5GSRegType'] = {'FOR': 1, 'Value': 1}
+    RegIEs['5GSID'] = {'spare': 0, 'Fmt': 0, 'spare': 0, 'Type': 1, 'Value': {'PLMN': ue.mcc + ue.mnc,
+                                                                              'RoutingInd': b'\x00\x00', 'spare': 0, 'ProtSchemeID': 0, 'HNPKID': 0, 'Output': encode_bcd(ue.msin)}}
+    RegIEs['UESecCap'] = {'5G-EA0': 1, '5G-EA1_128': 1, '5G-EA2_128': 1, '5G-EA3_128': 1, '5G-EA4': 0, '5G-EA5': 0, '5G-EA6': 0, '5G-EA7': 0, '5G-IA0': 1, '5G-IA1_128': 1, '5G-IA2_128': 1, '5G-IA3_128': 1, '5G-IA4': 0, '5G-IA5': 0,
+                          '5G-IA6': 0, '5G-IA7': 0, 'EEA0': 1, 'EEA1_128': 1, 'EEA2_128': 1, 'EEA3_128': 1, 'EEA4': 0, 'EEA5': 0, 'EEA6': 0, 'EEA7': 0, 'EIA0': 1, 'EIA1_128': 1, 'EIA2_128': 1, 'EIA3_128': 1, 'EIA4': 0, 'EIA5': 0, 'EIA6': 0, 'EIA7': 0}
+    RegIEs['5GSUpdateType'] = {
+        'EPS-PNB-CIoT': 0, '5GS-PNB-CIoT': 0, 'NG-RAN-RCU': 0, 'SMSRequested': 0}
+    RegIEs['5GMMCap'] = {'SGC': 0, '5G-HC-CP-CIoT': 0, 'N3Data': 0,
+                         '5G-CP-CIoT': 0, 'RestrictEC': 0, 'LPP': 0, 'HOAttach': 0, 'S1Mode': 0}
+    RegIEs['NSSAI'] = [{'SNSSAI': s} for s in ue.nssai]
+
+    RegMsg = FGMMRegistrationRequest(val=RegIEs)
+    IEs['5GMMHeader'] = { 'EPD': 126, 'spare': 0, 'SecHdr': 0 }
+    IEs['IMEISV'] = {'Type': FGSIDTYPE_IMEISV, 'Digit1': int(ue.imeiSv[0]), 'Digits': ue.imeiSv[1:]}
+    IEs['NASContainer'] = { }
+    
+    Msg = FGMMSecurityModeComplete(val=IEs)
+    Msg['NASContainer']['V'].set_val(RegMsg.to_bytes())
+    ue.MsgInBytes = Msg.to_bytes()
+    # Encrypt NAS message
+    SecMsg = security_prot_encrypt(ue, Msg)
+    logger.debug(f"UE {ue.supi} sending security_mode_complete")
+    ue.set_state(FGMMState.SECURITY_MODE_INITIATED)
+    return SecMsg
---
+++ src/UEUtils.py
@@ -0,0 +1,59 @@
+import logging
+from enum import IntEnum
+from binascii import unhexlify, hexlify
+from pycrate_mobile.TS24008_IE import encode_bcd
+from pycrate_mobile.TS24501_IE import FGSIDTYPE_IMEISV
+from pycrate_mobile.NAS import FGMMRegistrationRequest, FGMMMODeregistrationRequest, FGMMRegistrationComplete, FGMMAuthenticationResponse, FGMMSecProtNASMessage, FGMMSecurityModeComplete
+from pycrate_mobile.NAS5G import parse_NAS5G
+from CryptoMobile.Milenage import Milenage, make_OPc
+from CryptoMobile.conv import conv_501_A2, conv_501_A4, conv_501_A6, conv_501_A7, conv_501_A8
+
+logger = logging.getLogger('__UESim__')
+
+def byte_xor(ba1, ba2):
+    """ XOR two byte strings """
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+
+class FGMMState(IntEnum):
+    """ 5GMM State: 3GPP TS 24.501 version 16.9.0 Release 16, Section 5. """
+    NULL = 0
+    DEREGISTERED = 1
+    REGISTERED_INITIATED = 2
+    REGISTERED = 3
+    DEREGISTERED_INITIATED = 4
+    SERVICE_REQUEST_INITIATED = 5
+    # Not in 3GPP TS 24.501 version 16.9.0 Release 16, Section 5.
+    AUTHENTICATED_INITIATED = 6
+    AUTHENTICATED = 7
+    SECURITY_MODE_INITIATED = 8
+    SECURITY_MODE_COMPLETED = 9
+    # The states are for compliance test
+    FAIL = 10
+    PASS = 11
+    # Last state indicate number of states
+    FGMM_STATE_MAX = 12
+
+def security_prot_encrypt(ue, Msg):
+    IEs = {}
+    IEs['5GMMHeaderSec'] = { 'EPD': 126, 'spare': 0, 'SecHdr': 4 }
+    SecMsg = FGMMSecProtNASMessage(val=IEs)
+    SecMsg['NASMessage'].set_val(Msg.to_bytes())
+    SecMsg.encrypt(key=ue.k_nas_enc, dir=0, fgea=1, seqnoff=0, bearer=1)
+    SecMsg.mac_compute(key=ue.k_nas_int, dir=0, fgia=1, seqnoff=0, bearer=1)
+    return SecMsg
+
+def security_prot_decrypt(Msg, ue):
+    # Msg, err = parse_NAS5G(data)
+    # if err:
+    #     return
+    # check if message is encrypted
+    if Msg['5GMMHeaderSec']['SecHdr'].get_val() == 2:
+        # decrypt message
+        Msg.decrypt(ue.k_nas_enc, dir=1, fgea=1, seqnoff=0, bearer=1)
+        Msg, err = parse_NAS5G(Msg._dec_msg)
+        if err:
+            return None
+        return Msg
+    else:
+        return Msg
